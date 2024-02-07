e4941bf3580e009fad6d4f0e01ef761c50710e7e
Aloysio Rabello <aloysio.rabello@gmail.com>
2024-02-07 12:16:21 -0300

    feat: create views for tag creator with types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,14 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        product_code = body["product_code"]
+
+        #validação
+        print("Estou na minha view.")
+
+        #retorno
+        return HttpResponse(status_code=200, body={"Ola": "Mundo"})
+        
